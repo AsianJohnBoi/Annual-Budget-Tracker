7772263a5a83c572aae8ca2b2b51236ea84839f1
AsianJohnBoi <Jbssantias@hotmail.com>
2020-08-25 11:06:22 +1000

    merge auth

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 
 const { getAllTransactionalData } = require('./requests/transactions');
+const { authorize } = require('./requests/authGoogle');
+const { mapExpenseSheet } = require('./requests/sheet');
+
 const CALC_YEAR = true;
 const spendingAccount = process.env.UP_SPENDING_ACC;
 
@@ -12,11 +15,20 @@ const commonDescriptions = [
 if (CALC_YEAR) {
     (async () => {
         const tsns = await getAllTransactionalData('2020-08-01T00:00:00.52z', '2020-12-31T00:00:00.52z');
-        
+
         // filter
-        const spendingTsns = tsns.filter(e => 
+        const spendingTsns = tsns.filter(e =>
             e.relationships.account.data.id === spendingAccount
             && !commonDescriptions.includes(e.attributes.description)
         )
+
+        // Authorise user
+        const auth = await authorize();
+
+        // get data from the spreadsheet
+        const expenseCells = await mapExpenseSheet(auth);
+
+        // save to spreadsheet
+
     })()
 }
